a7200825a022e82e8c4d7810e4d74a13089a2d16
sseepun <sarun_sla@hotmail.com>
2021-04-16 14:00:39 +0700

    Init data table component

---
+++ src/components/DataTable.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="table-options">
+    <div v-if="orders.length" class="option">
+      จัดเรียงตาม: 
+      <select @input="(event)=>doOrder(event.target.value)">
+        <option 
+          v-for="(order, index) in orders" :key="order.key" :value="order.key" 
+          :selected="index == 0"
+        >
+          {{order.text}}
+        </option>
+      </select>
+    </div>
+    <div v-if="search.length > 0" class="option">
+      <div class="form-group">
+        <div class="prepend">
+          <input type="text" placeholder="ค้นหารายการ"
+            @input="(event)=>doSearch(event.target.value)" 
+          />
+          <div class="icon">
+            <img src="/assets/img/icon/search.svg" alt="Image Icon" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="option">
+      แสดง 
+      {{Math.min(selfFilteredRows.length, (selfPage - 1) * pp + 1)}} – 
+      {{Math.min(selfFilteredRows.length, selfPage * pp + 1)}} จากทั้งหมด 
+      {{selfFilteredRows.length}} 
+      รายการ
+    </div>
+    <div class="option">
+      <a href="javascript:" @click="changePage(-1)" class="btn-chev" :class="{ 'disabled': selfPage == 1 }">
+        <img src="/assets/img/icon/caret-left.svg" alt="Image Icon" />
+      </a>
+      <a href="javascript:" @click="changePage(1)" class="btn-chev" :class="{ 'disabled': selfPage == selfMaxPage }">
+        <img src="/assets/img/icon/caret-right.svg" alt="Image Icon" />
+      </a>
+    </div>
+  </div>
+  <div class="table-wrapper">
+    <table class="table">
+      <thead>
+        <tr>
+          <th v-for="col in columns" :key="col.key">
+            {{col.text}}
+          </th>
+        </tr>
+      </thead>
+      <tbody v-if="selfRows.length">
+        <tr v-for="(row, index) in selfRows" :key="index">
+          <td v-for="col in columns" :key="col.key">
+            <div v-html="highlight(col.key, row[col.key].text)"></div>
+          </td>
+        </tr>
+      </tbody>
+      <tbody v-else>
+        <tr>
+          <td class="text-center" :colspan="columns.length">
+            ไม่พบข้อมูลที่ค้นหา
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataTable',
+  props: {
+    columns: { type: Array, default: [] },
+    rows: { type: Array, default: [] },
+    page: { type: Number, default: 1 },
+    pp: { type: Number, default: 10 },
+    search: { type: Array, default: [] },
+    orders: { type: Array, default: [] }
+  },
+  data() {
+    return {
+      selfPage: this.page,
+      selfMaxPage: Math.ceil(this.rows.length / this.pp),
+      selfFilteredRows: [...this.rows],
+      selfRows: [...this.rows].slice(0, this.page * this.pp),
+      selfSearch: ''
+    }
+  },
+  methods: {
+    changePage(val) {
+      this.selfPage += val;
+      this.selfPage = Math.max(1, this.selfPage);
+      this.selfPage = Math.min(this.selfMaxPage, this.selfPage);
+      this.selfRows = this.selfFilteredRows.slice(
+        (this.selfPage - 1) * this.pp, this.selfPage * this.pp
+      );
+    },
+    doSearch(val) {
+      this.selfSearch = val;
+      if(val){
+        var search = this.search;
+        this.selfFilteredRows = this.rows.filter(function(item){
+          var valid = false;
+          for(var s of search){
+            if(item[s] && item[s].text.indexOf(val) > -1){
+              valid = true;
+              break;
+            }
+          }
+          return valid;
+        });
+      }else{
+        this.selfFilteredRows = [...this.rows];
+      }
+      this.selfPage = 1;
+      this.selfMaxPage = Math.ceil(this.selfFilteredRows.length / this.pp);
+      this.selfRows = this.selfFilteredRows.slice(
+        (this.selfPage - 1) * this.pp, this.selfPage * this.pp
+      );
+      return true;
+    },
+    doOrder(val) {
+      console.log('asd')
+      if(val.indexOf('-asc') > -1){
+        val = val.replace('-asc', '');
+        this.selfFilteredRows.sort(function(a, b){
+          if(!a[val] || !b[val]){
+            return false;
+          }else{
+            return (a[val].text > b[val].text)? -1: ((b[val].text > a[val].text)? 1: 0);
+          }
+        });
+      }else if(val.indexOf('-desc') > -1){
+        val = val.replace('-desc', '');
+        this.selfFilteredRows.sort(function(a, b){
+          if(!a[val] || !b[val]){
+            return false;
+          }else{
+            return (a[val].text > b[val].text)? 1: ((b[val].text > a[val].text)? -1: 0);
+          }
+        });
+      }
+      this.selfRows = this.selfFilteredRows.slice(
+        (this.selfPage - 1) * this.pp, this.selfPage * this.pp
+      );
+      return true;
+    },
+    highlight(key, text) {
+      if(this.search.indexOf(key) > -1 && this.selfSearch){
+        return text.replace(
+          new RegExp(this.selfSearch, 'ig'), 
+          '<span style="background:blue;">'+this.selfSearch+'</span>'
+        );
+      }else{
+        return text;
+      }
+    }
+  },
+  created() {
+    if(this.orders.length){
+      this.doOrder(this.orders[0].key);
+    }
+  }
+}
+</script>
--- src/views/user/Cases.vue
+++ src/views/user/Cases.vue
@@ -35,7 +35,14 @@
         <div class="tab-contents" data-aos="fade-up" data-aos-delay="150">
 
           <div class="tab-content" :class="{ 'active': tabActiveIndex == 0 }">
-            Tab 0
+            <DataTable 
+              :columns="columns1" :rows="rows1" 
+              :search="[ 'sent_to', 'case_id', 'hn', 'name', 'thai_id' ]" 
+              :orders="[
+                { key: 'sent_date-desc', text: 'วันที่นำเข้า (ใหม่สุด)' },
+                { key: 'sent_date-asc', text: 'วันที่นำเข้า (เก่าสุด)' },
+              ]"
+            />
           </div>
           
           <div class="tab-content" :class="{ 'active': tabActiveIndex == 1 }">
@@ -67,21 +74,61 @@
 import Topnav from '../../components/Topnav';
 import PopupOptions from '../../components/PopupOptions';
 import Tabs01 from '../../components/Tabs01';
+import DataTable from '../../components/DataTable';
 
 export default {
   name: 'UserCasesPage',
   components: {
     Topnav,
     PopupOptions,
-    Tabs01
+    Tabs01,
+    DataTable
   },
   data() {
     return {
-      isOpenedOptions: false
+      isOpenedOptions: false,
+      columns1: [
+        { key: 'sent_to', text: 'นำส่ง' },
+        { key: 'sent_date', text: 'วันที่นำเข้า' },
+        { key: 'case_id', text: 'ID Case' },
+        { key: 'hn', text: 'HN' },
+        { key: 'an', text: 'AN' },
+        { key: 'name', text: 'ชื่อ นามสกุล' },
+        { key: 'thai_id', text: 'เลขบัตรประชาชน' },
+        { key: 'card', text: 'ใบส่งตรวจ' },
+        { key: 'status', text: 'สถานะ' },
+      ],
+      rows1: []
     }
   },
   created() {
     AOS.init({ easing: 'ease-in-out-cubic', duration: 750, once: true, offset: 10 });
+
+    for(var i=0; i<22; i++){
+      this.rows1.push({
+        sent_to: { text: 'ชิ้นเนื้อ' },
+        sent_date: { text: '05/12/2563, 10:34' },
+        case_id: { text: 'FI63-01526' },
+        hn: { text: '1088052' },
+        an: { text: '1254454' },
+        name: { text: 'สงกรานต์ สุขุมมณีวงศ์' },
+        thai_id: { text: '1-9698-00169-84-9' },
+        card: { text: 'S21-00001' },
+        status: { text: 'รอรับเข้าบริการ' }
+      });
+      this.rows1.push({
+        sent_to: { text: 'เซลล์วิทยา' },
+        sent_date: { text: '20/11/2563, 14:05' },
+        case_id: { text: 'FI63-07660' },
+        hn: { text: '1005375' },
+        an: { text: '1254445' },
+        name: { text: 'นวรัตร์ ระเบียบธรรม' },
+        thai_id: { text: '1-4505-53700-28-4' },
+        card: { text: 'CG21-00001' },
+        status: { text: 'รอรับเข้าบริการ' }
+      });
+    }
+
   },
   props: {
     tabActiveIndex: { type: Number, default: 0 }
